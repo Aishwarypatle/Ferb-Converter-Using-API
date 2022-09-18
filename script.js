96d0045cb972eb8bc11fc434d0849ce5b6c94a30
Aishwary Patle <61194375+Aishwarypatle@users.noreply.github.com>
2022-09-18 20:10:14 +0000

    added all JS code with API

---
+++ script.js
@@ -0,0 +1,24 @@
+let inputTxt = document.querySelector("#txt-input");
+let translateBtn = document.querySelector("#btn-translate");
+let Output = document.querySelector("#output");
+
+let serverURL = 'https://api.funtranslations.com/translate/ferb-latin.json';
+
+let getTranslation = (text) => {
+  return serverURL + '?' + 'text=' + text;
+}
+
+
+let errorHandling = (error) => {
+  console.log("error happened", error);
+  alert("Server Error,Please try after some time");
+}
+
+let clickEvent = () => {
+  fetch(getTranslation(inputTxt.value))
+    .then(response => response.json())
+    .then(json => Output.innerText = json.contents.translated)
+    .catch(errorHadling);
+
+}
+btnTranslate.addEventListener("click", clickEvent);
